0a65eb47f5982842b22b549b4d1080f4a86fc58a
André Gilbert <andre-anan.gilbert@sap.com>
2021-08-06 22:42:32 +0200

    added header

--- src/components/header.py
+++ src/components/header.py
@@ -2,6 +2,24 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 
-def header(page_name: str):
-    header = html.Div([])
+def header(page_name: str = 'Ordered Spend'):
+    header = html.Div([
+        html.Div([
+            html.H1(page_name, className='page-title'),
+            html.Div([
+                html.Div([
+                    html.P('Company Code:', className='filter-bar-label'),
+                    dcc.Input(id='input-company-code', type='search')
+                ],
+                         className='filter-bar-container'),
+                html.Div([html.P('Purchasing Organization:', className='filter-bar-label')],
+                         className='filter-bar-container'),
+                html.Div([html.P('Plant:', className='filter-bar-label')], className='filter-bar-container'),
+                html.Div([html.P('Material Group:', className='filter-bar-label')], className='filter-bar-container')
+            ],
+                     className='filter-bar')
+        ],
+                 className='main-content')
+    ],
+                      className='header')
     return header
--- src/index.py
+++ src/index.py
@@ -1,10 +1,10 @@
-import dash_core_components as dcc
 import dash_html_components as html
 
 from app import app
+from components.header import header
 from components.navbar import navbar
 
-app.layout = html.Div(navbar(app))
+app.layout = html.Div([navbar(app), header()])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
