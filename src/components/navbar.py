82c861a75a9cc0c030c63e616c40df2bbfda8b46
André Gilbert <andre-anan.gilbert@sap.com>
2021-08-06 21:01:33 +0200

    added navbar

--- src/components/navbar.py
+++ src/components/navbar.py
@@ -2,6 +2,16 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 
-def navbar():
-    navbar = html.Div(children=[html.Div(children='Logo & Nav'), html.Div(children='User')])
+def navbar(app):
+    navbar = html.Div([
+        html.Div([
+            html.Img(src=app.get_asset_url("logo.svg")),
+            html.Button(['Ordered Spend',
+                         html.Img(src=app.get_asset_url("arrow_down.svg"), className='arrow-down')],
+                        className='nav-container')
+        ],
+                 className='container'),
+        html.Div(html.P('SF', className='icon-text'), className='user-icon')
+    ],
+                      className='navbar')
     return navbar
--- src/index.py
+++ src/index.py
@@ -4,7 +4,7 @@ import dash_html_components as html
 from app import app
 from components.navbar import navbar
 
-app.layout = html.Div(children=navbar())
+app.layout = html.Div(navbar(app))
 
 if __name__ == "__main__":
     app.run_server(debug=True)
