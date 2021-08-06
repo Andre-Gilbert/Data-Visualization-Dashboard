330aabe4a75f8d8a1382c3da617a2ed136c11d58
André Gilbert <andre-anan.gilbert@sap.com>
2021-08-06 21:07:15 +0200

    added navbar

--- src/components/navbar.py
+++ src/components/navbar.py
@@ -5,13 +5,17 @@ import dash_html_components as html
 def navbar(app):
     navbar = html.Div([
         html.Div([
-            html.Img(src=app.get_asset_url("logo.svg")),
-            html.Button(['Ordered Spend',
-                         html.Img(src=app.get_asset_url("arrow_down.svg"), className='arrow-down')],
-                        className='nav-container')
+            html.Div([
+                html.Img(src=app.get_asset_url("logo.svg")),
+                html.Button(
+                    ['Ordered Spend',
+                     html.Img(src=app.get_asset_url("arrow_down.svg"), className='arrow-down')],
+                    className='nav-container')
+            ],
+                     className='container'),
+            html.Div(html.P('SF', className='icon-text'), className='user-icon')
         ],
-                 className='container'),
-        html.Div(html.P('SF', className='icon-text'), className='user-icon')
+                 className='main-content')
     ],
                       className='navbar')
     return navbar
