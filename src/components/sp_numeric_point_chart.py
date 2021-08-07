c588527c5c573965751186971f288981d2ee4fc0
André Gilbert <andre.gilbert.77110@gmail.com>
2021-08-07 21:38:15 +0200

    added numeric point chart

--- src/components/sp_numeric_point_chart.py
+++ src/components/sp_numeric_point_chart.py
@@ -2,5 +2,12 @@ import dash_html_components as html
 
 
 def sp_numeric_point_chart():
-    sp_numeric_point_chart = html.Div([])
+    sp_numeric_point_chart = html.Div([
+        html.Div([
+            html.H1('Delivery Deviation', className='npc-title'),
+            html.Div([html.P('Chart One'), html.P('Chart Two'),
+                      html.P('Chart Three')], className='npc-chart-container')
+        ],
+                 className='npc-container-one')
+    ])
     return sp_numeric_point_chart
