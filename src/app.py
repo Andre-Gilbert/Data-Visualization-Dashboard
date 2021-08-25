84068f293cb0d6679755fe8c180ad58835c5479c
André Gilbert <andre-anan.gilbert@sap.com>
2021-08-25 10:31:35 +0200

    refactored code

--- src/app.py
+++ src/app.py
@@ -1,13 +1,15 @@
 import dash
 import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__,
-                suppress_callback_exceptions=True,
-                external_stylesheets=[dbc.themes.BOOTSTRAP],
-                meta_tags=[{
-                    "name": "viewport",
-                    "content": "width=device-width, initial-scale=1.0"
-                }])
+app = dash.Dash(
+    __name__,
+    suppress_callback_exceptions=True,
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    meta_tags=[{
+        "name": "viewport",
+        "content": "width=device-width, initial-scale=1.0"
+    }],
+)
 
 app.title = "Dashboard"
 server = app.server
